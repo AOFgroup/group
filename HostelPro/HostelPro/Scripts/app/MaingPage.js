6e438a20c2aa74f7cef11861bc43a33d34b0b706
OleksandrKL <olkly85@gmail.com>
2014-12-03 08:58:21 +0100

    update

---
+++ HostelPro/HostelPro/Scripts/app/MaingPage.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    var window = $("#window"),
+        undo = $("#login")
+                .bind("click", function () {
+                    window.data("kendoWindow").open();
+                    undo.hide();
+                });
+
+    var onClose = function () {
+        undo.show();
+    
+
+    }
+
+    if (!window.data("kendoWindow")) {
+        window.kendoWindow({
+            width: "15%",
+            title: "LogIn",
+            actions: [
+                "Pin",
+                "Minimize",
+                "Maximize",
+                "Close"
+            ],
+            position:{
+
+                top:58,
+                left:735
+            },
+            close: onClose
+        });
+    }
+    function hide() {
+        $('.k-widget.k-window').hide();
+        $('#login').css('display','block')
+    }
+    $('#register').click(hide);
+
+
+});

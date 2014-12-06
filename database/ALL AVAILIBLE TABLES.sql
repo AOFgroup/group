dc278eede7d52b4bc81c09e5c31530e0e9c4b3e3
OleksandrKL <olkly85@gmail.com>
2014-12-06 18:24:23 +0100

    up

--- database/ALL AVAILIBLE TABLES.sql
+++ database/ALL AVAILIBLE TABLES.sql
@@ -2,8 +2,9 @@ ALTER PROCEDURE AvailibleBeds @DateStart date=null, @DateEnd date=null
 AS 
 DECLARE @DURATION int, @EXISTING DATE
 SET @DURATION=(DATEDIFF(DAY,@DateStart,@DateEnd))
-SELECT  B.ID AS [Bed Number], BB.DateEnd From dbo.BED as B 
+SELECT  B.ID AS [Bed Number],R.ID, BB.DateEnd From dbo.BED as B 
 left join BookingBed as BB on BB.BedId=B.ID
+left join Room as R on R.ID=B.RoomId
 where bb.DateStart is null or bb.DateEnd is null OR @DateStart>=BB.DateEnd AND (DATEADD(DAY,@DURATION,BB.DateStart))<=BB.DateEnd
 
 
@@ -12,9 +13,18 @@ where bb.DateStart is null or bb.DateEnd is null OR @DateStart>=BB.DateEnd AND (
 
 
 
+alter PROCEDURE CountBeds @DateStart date=null, @DateEnd date=null, @R_Id int=null
+AS 
+DECLARE @DURATION int, @EXISTING DATE
+SET @DURATION=(DATEDIFF(DAY,@DateStart,@DateEnd))
+SELECT b.ID, COUNT(b.RoomId)From dbo.BED as B 
+left join BookingBed as BB on BB.BedId=B.ID
+left join Room AS R ON R.ID=B.RoomId
+where bb.DateStart is null or bb.DateEnd is null OR @DateStart>=BB.DateEnd AND (DATEADD(DAY,@DURATION,BB.DateStart))<=BB.DateEnd
+and r.ID=@R_Id
+Group by b.ID
 
 
 
-SELECT B.ID AS [Bed Number],BB.DateStart ,BB.DateEnd, B.Price From dbo.BED as B 
-LEFT join BookingBed as BB on BB.BedId=B.ID
-where BB.DateStart>='2014-11-10' or BB.DateStart is null and bb.DateEnd<@Da
+exec AvailibleBeds '2014-12-12','2014-12-16'
+exec CountBeds '2014-12-07','2014-12-08',12
